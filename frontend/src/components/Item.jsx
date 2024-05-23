00719289b46211941d301da5e49c0e4c9421b5ad
knllydgmntz <knllydgmntz@gmail.com>
2024-05-23 19:12:15 +0800

    adding product display and context

--- frontend/src/components/Item.jsx
+++ frontend/src/components/Item.jsx
@@ -6,7 +6,7 @@ const Item = ({ id, name, image, new_price, old_price }) => {
     <div className="rounded-xl overflow-hidden shadow-lg">
       <div className="relative flexCenter group overflow-hidden transition-all duration-100">
         <Link
-          to={`product/${id}`}
+          to={`/product/${id}`}
           className="h-12 w-12 bg-white rounded-full flexCenter absolute top-1/2 bottom-1/2 !py-2 z-20 scale-0 group-hover:scale-100 transistion-all duration-700"
         >
           <FaSearch className="scale-125 hover:rotate-90 hover:scale-125 transition-all duration-200" />
