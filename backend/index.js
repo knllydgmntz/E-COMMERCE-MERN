e4f737165d988d3adfe0f33947f1cdc20b543368
knllydgmntz <knllydgmntz@gmail.com>
2024-05-28 00:20:04 +0800

    adding functions in login signup create schema in mongodb

--- backend/index.js
+++ backend/index.js
@@ -146,6 +146,55 @@ const User = mongoose.model("User", {
 });
 
 // creating endpoint for registering the user
+app.post("/signup", async (req, res) => {
+  let check = await User.findOne({ email: req.body.email });
+  if (check) {
+    return res.status(400).json({
+      success: false,
+      errors: "Existing user found with same email address",
+    });
+  }
+  let cart = {};
+  for (let i = 0; i < 300; i++) {
+    cart[i] = 0;
+  }
+  const user = new User({
+    name: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    cartData: cart,
+  });
+  await user.save();
+
+  const data = {
+    user: {
+      id: user.id,
+    },
+  };
+  const token = jwt.sign(data, "secret_ecom");
+  res.json({ success: true, token });
+});
+
+// creating endpoint for user login
+app.post("/login", async (req, res) => {
+  let user = await User.findOne({ email: req.body.email });
+  if (user) {
+    const passMatch = req.body.password === user.password;
+    if (passMatch) {
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const token = jwt.sign(data, "secret_ecom");
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, errors: "Wrong Password" });
+    }
+  } else {
+    res.json({ success: false, errors: "Wrong Email Address" });
+  }
+});
 
 app.listen(port, (error) => {
   if (!error) {
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,60 +5,114 @@ import { FaEyeSlash, FaEye } from "react-icons/fa6";
 
 const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const [state, setState] = useState("Login");
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    email: "",
+  });
+
+  const changeHandler = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const login = async () => {
+    console.log("Login function executed", formData);
+  };
+
+  const signup = async () => {
+    console.log("signup function executed", formData);
+  };
   return (
     <section className="max_padd_container flexCenter flex-col pt-32">
       <div className="max-w-[555px] h-[600px] bg-white m-auto px-14 py-10 rounded-md">
-        <h3 className="h3 font-semibold">Sign Up</h3>
+        <h3 className="h3 font-semibold">{state}</h3>
         <p className="font-medium text-gray-500 mt-4">
           Please fill up details.
         </p>
         <div className="mt-8">
-          <div>
-            <label className=" font-medium">Full Name</label>
-            <input
-              type="text"
-              className="w-full border-2 bg-slate-900/5 rounded-xl p-4 mt-1"
-              placeholder="Enter your full name"
-            />
+          <div className="mb-5">
+            {state === "Sign Up" ? (
+              <input
+                name="username"
+                value={formData.username}
+                onChange={changeHandler}
+                type="text"
+                className="w-full border-2 bg-slate-900/5 rounded-xl p-4 mt-1"
+                placeholder="Enter your name"
+              />
+            ) : (
+              ""
+            )}
           </div>
-          <div>
-            <label className=" font-medium">Email</label>
+          <div className="mb-5">
             <input
+              name="email"
+              value={formData.email}
+              onChange={changeHandler}
               type="email"
               className="w-full border-2 bg-slate-900/5 rounded-xl p-4 mt-1"
               placeholder="Enter your email"
             />
           </div>
           <div className="relative">
-            <label className=" font-medium">Password</label>
             <input
+              name="password"
+              value={formData.password}
+              onChange={changeHandler}
               type={!showPassword ? "password" : "text"}
               className="w-full border-2 bg-slate-900/5 rounded-xl p-4 mt-1"
               placeholder="Enter your password"
             />
             <div
-              className="absolute top-[50px] right-4 cursor-pointer"
+              className="absolute top-[25px] right-4 cursor-pointer"
               onClick={() => setShowPassword(!showPassword)}
             >
               {!showPassword ? <FaEyeSlash /> : <FaEye />}
             </div>
           </div>
-
-          <div className="flex justify-between items-center"></div>
         </div>
         <div className="mt-8 flex flex-col gap-y-4">
-          <button className="py-3 rounded-xl border border-green-90 bg-green-90 hover:bg-black text-white text-lg font-bold active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out">
-            Create account
+          <button
+            onClick={() => {
+              state === "Login" ? login() : signup();
+            }}
+            className="py-3 rounded-xl border border-green-90 bg-green-90 hover:bg-black text-white text-lg font-bold active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out"
+          >
+            Continue
           </button>
         </div>
-        <div className="mt-6 flex justify-center items-center ">
-          <p className="text-black font-bold">
-            Already have an account?
-            <span className="text-secondary underline cursor-pointer ml-1">
-              Login
-            </span>
-          </p>
-        </div>
+
+        {state === "Sign Up" ? (
+          <div className="mt-6 flex justify-center items-center ">
+            <p className="text-black font-bold">
+              Already have an account?
+              <span
+                onClick={() => {
+                  setState("Login");
+                }}
+                className="text-secondary underline cursor-pointer ml-1"
+              >
+                Login
+              </span>
+            </p>
+          </div>
+        ) : (
+          <div className="mt-6 flex justify-center items-center ">
+            <p className="text-black font-bold">
+              Create an account?
+              <span
+                onClick={() => {
+                  setState("Sign Up");
+                }}
+                className="text-secondary underline cursor-pointer ml-1"
+              >
+                Click here
+              </span>
+            </p>
+          </div>
+        )}
+
         <div className="flexCenter mt-3 gap-2">
           <input type="checkbox" name="" id="" />
           <p>By continuing, i agree to the terms of use & privacy policy.</p>
